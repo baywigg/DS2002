6ab928891a77393205a22edac9c75aff744cbf0c
Bay Wiggins <pax8nw@virginia.edu>
2024-11-25 16:42:36 -0500

    removed final

--- final/ETLPipeline/extract/get_data_from_file.py
+++
@@ -1,5 +0,0 @@
-import csv
-import pandas as pd
-
-def get_gapminder_data_from_file():
-    return pd.read_csv("ETLPipeline/data/gapminder.csv")
--- final/ETLPipeline/transform/transform_functions.py
+++
@@ -1,6 +0,0 @@
-from . import country_name_mapping
-
-def transform_country_names(row):
-    if row["Entity"] in country_name_mapping:
-        row["Entity"] = country_name_mapping[row["Entity"]]
-    return row
